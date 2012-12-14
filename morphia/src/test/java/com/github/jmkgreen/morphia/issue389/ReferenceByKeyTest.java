8ebbe9fff3e5393267b4824970ee6538efd5818d
anton.kopylov <anton.copylov@gmail.com>
2012-12-14 12:01:14 +0200

    Fixed test resolver to work properly with non-id reference fields

--- morphia/src/test/java/com/github/jmkgreen/morphia/issue389/ReferenceByKeyTest.java
+++ morphia/src/test/java/com/github/jmkgreen/morphia/issue389/ReferenceByKeyTest.java
@@ -25,7 +25,9 @@ public class ReferenceByKeyTest extends TestBase {
     static class ReferenceByKeyResolver implements ReferenceObjectResolver {
 
         public DBObject resolveObject(DBObject dbObject, DBRef dbRef, MappedField mf) {
-            return dbRef.getDB().getCollection(dbRef.getRef()).findOne(dbRef.getId());
+            String field = mf.getAnnotation(Reference.class).referenceField();
+            DBObject query = new BasicDBObject(field, dbRef.getId()) ;
+            return dbRef.getDB().getCollection(dbRef.getRef()).findOne(query);
         }
     }
     static class Father {
@@ -37,24 +39,29 @@ public class ReferenceByKeyTest extends TestBase {
     }
     static class Son {
         @Id ObjectId id;
+
         String name;
 
-        Son(String name) {
+        Son() {
+        }
+
+        public Son(String name) {
             this.name = name;
         }
     }
     @Test
     public void testEntityLoadedByKey() throws Exception {
         morphia.map(Son.class, Father.class);
-        ds.save(new Son("first"), new Son("second"));
-        BasicDBList sons = new BasicDBList();
-        sons.addAll(Arrays.asList(createDBRef("first"), createDBRef("second"), createDBRef("third")));
-        BasicDBObject father = new BasicDBObject("sons", sons);
-        ds.getCollection(Father.class).insert(father, WriteConcern.SAFE);
 
-        Object id = father.get("_id");
 
-        Father father1 = ds.get(Father.class, id);
+        Son first = new Son("first");
+        ds.save(first, new Son("second"));
+        Father father = new Father();
+        father.sons = Arrays.asList(first);
+
+        ds.save(father);
+
+        Father father1 = ds.get(Father.class, father.id);
         assertNotNull(father1);
         assertNotNull(father1.sons);
         assertEquals(2, father1.sons.size());
